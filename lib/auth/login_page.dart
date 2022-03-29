fc199e630371a9bbe652458e7c52d785e2899a25
tatarmagda <tatar.magdalena@wp.pl>
2022-03-29 18:36:34 +0200

    chowanie klawiaury na on tap

--- lib/auth/login_page.dart
+++ lib/auth/login_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ffi';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -23,135 +25,135 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    print(Provider.of<AuthState>(context).userChanges);
-
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black, actions: <Widget>[
-        OutlinedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => Home()),
-            );
-          },
-          child: Text(
-            "POMIŃ",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            side: BorderSide(
-              width: 2,
-              color: Color.fromARGB(255, 255, 255, 255),
-            ),
-          ),
-        )
-      ]),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 50, 8, 20),
-                    child: Image.asset(
-                      "assets/3657586_print_1.png",
-                      scale: 3,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 90),
-                    child: MyTextWidget(
-                      color: Colors.white,
-                      size: 55,
-                      text: "Polmem",
-                    ),
-                  ),
-                  completeLogin(),
-                  Padding(padding: EdgeInsets.only(top: 20)),
-                  completePassword(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20),
-                  ),
-                  Center(
-                    child: TextButton(
-                      child: Text(
-                        _signUp ? "Zarejestruj" : "Zaloguj",
-                        style: TextStyle(
-                            fontSize: 24,
-                            color: Color.fromARGB(255, 255, 255, 255)),
-                      ),
-                      onPressed: _signUp
-                          ? () {
-                              if (_formKey.currentState!.validate()) {
-                                Provider.of<AuthState>(
-                                  context,
-                                  listen: false,
-                                )
-                                    .signOnWithEmail(
-                                        email: _emailController.text.trim(),
-                                        password: _passwordController.text)
-                                    .whenComplete(() {
-                                  if (context.read<User?>() != null) {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => Home(),
-                                        ));
-                                  }
-                                });
-                              }
-                            }
-                          : () {
-                              if (_formKey.currentState!.validate()) {
-                                Provider.of<AuthState>(
-                                  context,
-                                  listen: false,
-                                )
-                                    .signInWithEmail(
-                                        email: _emailController.text.trim(),
-                                        password: _passwordController.text)
-                                    .whenComplete(() {
-                                  if (context.read<User?>() != null) {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => Home(),
-                                        ));
-                                  }
-                                });
-                              }
-                            },
-                    ),
-                  ),
-                  Center(
-                    child: TextButton(
-                      child: Text(
-                        !_signUp ? "Zarejestruj" : "Zaloguj",
-                        style: TextStyle(
-                          color: Color(0xFF1B6569),
-                          fontSize: 24,
+    return GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          appBar: AppBar(backgroundColor: Colors.black, actions: <Widget>[
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
+              },
+              child: Text(
+                "POMIŃ",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(
+                  width: 2,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            )
+          ]),
+          body: Form(
+            key: _formKey,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 40, 8, 20),
+                        child: Image.asset(
+                          "assets/3657586_print_1.png",
+                          scale: 3,
                         ),
                       ),
-                      onPressed: () {
-                        setState(() {
-                          _signUp = !_signUp;
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 90),
+                        child: MyTextWidget(
+                          color: Colors.white,
+                          size: 55,
+                          text: "Polmem",
+                        ),
+                      ),
+                      completeLogin(),
+                      Padding(padding: EdgeInsets.only(top: 20)),
+                      completePassword(),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
+                      ),
+                      Center(
+                        child: TextButton(
+                          child: Text(
+                            _signUp ? "Zarejestruj" : "Zaloguj",
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                          onPressed: _signUp
+                              ? () {
+                                  if (_formKey.currentState!.validate()) {
+                                    Provider.of<AuthState>(
+                                      context,
+                                      listen: false,
+                                    )
+                                        .signOnWithEmail(
+                                            email: _emailController.text.trim(),
+                                            password: _passwordController.text)
+                                        .whenComplete(() {
+                                      if (context.read<User?>() != null) {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => Home(),
+                                            ));
+                                      }
+                                    });
+                                  }
+                                }
+                              : () {
+                                  if (_formKey.currentState!.validate()) {
+                                    Provider.of<AuthState>(
+                                      context,
+                                      listen: false,
+                                    )
+                                        .signInWithEmail(
+                                            email: _emailController.text.trim(),
+                                            password: _passwordController.text)
+                                        .whenComplete(() {
+                                      if (context.read<User?>() != null) {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => Home(),
+                                            ));
+                                      }
+                                    });
+                                  }
+                                },
+                        ),
+                      ),
+                      Center(
+                        child: TextButton(
+                          child: Text(
+                            !_signUp ? "Zarejestruj" : "Zaloguj",
+                            style: TextStyle(
+                              color: Color(0xFF1B6569),
+                              fontSize: 24,
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _signUp = !_signUp;
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 
   Widget completeLogin() {
