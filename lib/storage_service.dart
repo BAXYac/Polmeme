0ae9c89f335c28dd8a1dc084aa014ecf11fbb6be
tatarmagda <tatar.magdalena@wp.pl>
2022-04-08 10:09:19 +0200

    zakończenie classy Storage

--- lib/storage_service.dart
+++ lib/storage_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
 
 class Storage {
   final firebase_storage.FirebaseStorage storage =
@@ -11,7 +12,7 @@ class Storage {
 
     try {
       await storage.ref("test/$fileName").putFile(file);
-    } catch (e) {
+    } on firebase_core.FirebaseException catch (e) {
       print(e);
     }
   }
